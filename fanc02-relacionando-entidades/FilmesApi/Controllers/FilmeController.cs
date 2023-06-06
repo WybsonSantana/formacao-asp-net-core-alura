0436c8528e1f08e6b8f0b94aebb0894b697ff394
WybsonSantana <wybson@hotmail.com>
2023-06-06 01:01:58 -0300

    fanc02 - aula 1 - Adiciona o projeto

---
+++ fanc02-relacionando-entidades/FilmesApi/Controllers/FilmeController.cs
@@ -0,0 +1,118 @@
+﻿using AutoMapper;
+using FilmesApi.Data;
+using FilmesApi.DTOs;
+using FilmesApi.Models;
+using Microsoft.AspNetCore.JsonPatch;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FilmesApi.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class FilmeController : ControllerBase
+{
+
+    private FilmeContext _context;
+    private IMapper _mapper;
+
+    public FilmeController(FilmeContext context, IMapper mapper)
+    {
+        _context = context;
+        _mapper = mapper;
+    }
+
+    /// <summary>
+    /// Lista todos os filmes do banco de dados
+    /// </summary>
+    /// <returns>IActionResult</returns>
+    /// <response code="200">Caso a consulta seja feita com sucesso</response>
+    [HttpGet]
+    public IEnumerable<ReadFilmeDto> listaFilmes(
+        [FromQuery] int skip = 0,
+        [FromQuery] int take = 50)
+    {
+        return _mapper.Map<List<ReadFilmeDto>>(_context.Filmes.Skip(skip).Take(take));
+    }
+
+    [HttpGet("{id}")]
+    public IActionResult ListaFilmePorId(int id)
+    {
+        var filme = _context.Filmes.FirstOrDefault(filme => filme.Id == id);
+
+        if (filme == null) return NotFound();
+
+        var filmeDto = _mapper.Map<ReadFilmeDto>(filme);
+
+        return Ok(filmeDto);
+    }
+
+    /// <summary>
+    /// Adiciona um filme ao banco de dados
+    /// </summary>
+    /// <param name="filmeDto">Objeto com os campos necessários para criação de um filme</param>
+    /// <returns>IActionResult</returns>
+    /// <response code="201">Caso inserção seja feita com sucesso</response>
+    [HttpPost]
+    //[ProducesResponseType(StatusCodes.Status200OK)]
+    public IActionResult AdcionaFilme([FromBody] CreateFilmeDto filmeDto)
+    {
+        var filme = _mapper.Map<Filme>(filmeDto);
+
+        _context.Filmes.Add(filme);
+        _context.SaveChanges();
+
+        return CreatedAtAction(
+            nameof(ListaFilmePorId),
+            new { id = filme.Id },
+            value: filme
+            );
+    }
+
+    [HttpPut("{id}")]
+    public IActionResult atualizaFilme(int id, [FromBody] UpdateFilmeDto filmeDto)
+    {
+        var filme = _context.Filmes.FirstOrDefault(filme => filme.Id == id);
+
+        if (filme == null) return NotFound();
+
+        _mapper.Map(filmeDto, filme);
+        _context.SaveChanges();
+
+        return NoContent();
+    }
+
+    [HttpPatch("{id}")]
+    public IActionResult atualizaFilmeParcial(int id, JsonPatchDocument<UpdateFilmeDto> filmePatch)
+    {
+        var filme = _context.Filmes.FirstOrDefault(filme => filme.Id == id);
+
+        if (filme == null) return NotFound();
+
+        var filmeParaAtualizar = _mapper.Map<UpdateFilmeDto>(filme);
+
+        filmePatch.ApplyTo(filmeParaAtualizar, ModelState);
+
+        if (!TryValidateModel(filmeParaAtualizar))
+        {
+            return ValidationProblem(ModelState);
+        }
+
+        _mapper.Map(filmeParaAtualizar, filme);
+        _context.SaveChanges();
+
+        return NoContent();
+    }
+
+    [HttpDelete("{id}")]
+    public IActionResult deletaFilme(int id)
+    {
+        var filme = _context.Filmes.FirstOrDefault(filme => filme.Id == id);
+
+        if (filme == null) return NotFound();
+
+        _context.Remove(filme);
+        _context.SaveChanges();
+
+        return NoContent();
+    }
+}
---
+++ fanc02-relacionando-entidades/FilmesApi/Profiles/FilmeProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using FilmesApi.DTOs;
+using FilmesApi.Models;
+
+public class FilmeProfile : Profile
+{
+    public FilmeProfile()
+    {
+        CreateMap<CreateFilmeDto, Filme>();
+        CreateMap<UpdateFilmeDto, Filme>();
+        CreateMap<Filme, UpdateFilmeDto>();
+        CreateMap<Filme, ReadFilmeDto>();
+    }
+}
