bc2d69ef8263b023d9b35ed4c28afa37aeaa4bc5
WybsonSantana <wybson@hotmail.com>
2023-06-10 20:21:41 -0300

    fanc02 - aula 5 - Consultas com LINQ

--- fanc02-relacionando-entidades/FilmesApi/Controllers/FilmeController.cs
+++ fanc02-relacionando-entidades/FilmesApi/Controllers/FilmeController.cs
@@ -29,9 +29,17 @@ public class FilmeController : ControllerBase
     [HttpGet]
     public IEnumerable<ReadFilmeDto> listaFilmes(
         [FromQuery] int skip = 0,
-        [FromQuery] int take = 50)
+        [FromQuery] int take = 50,
+        [FromQuery] string? nomeCinema = null)
     {
-        return _mapper.Map<List<ReadFilmeDto>>(_context.Filmes.Skip(skip).Take(take).ToList());
+        if (nomeCinema == null)
+        {
+            return _mapper.Map<List<ReadFilmeDto>>(_context.Filmes.Skip(skip).Take(take).ToList());
+        }
+
+        return _mapper.Map<List<ReadFilmeDto>>(_context.Filmes.Skip(skip).Take(take).Where(filme =>
+        filme.sessoes.Any(sessao =>
+        sessao.Cinema.Nome == nomeCinema)).ToList());
     }
 
     [HttpGet("{id}")]
