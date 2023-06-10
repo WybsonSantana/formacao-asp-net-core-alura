e1b4eac6315f975cd295879fc5a0ebb1b095dc0d
WybsonSantana <wybson@hotmail.com>
2023-06-10 13:33:02 -0300

    fanc02 - aula 4 - Relacionando com ModelBuilder

--- fanc02-relacionando-entidades/FilmesApi/Data/FilmeContext.cs
+++ fanc02-relacionando-entidades/FilmesApi/Data/FilmeContext.cs
@@ -14,4 +14,20 @@ public class FilmeContext : DbContext
     {
 
     }
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        builder.Entity<Sessao>()
+            .HasKey(sessao => new { sessao.FilmeId, sessao.CinemaId });
+
+        builder.Entity<Sessao>()
+            .HasOne(sessao => sessao.Cinema)
+            .WithMany(cinema => cinema.Sessoes)
+            .HasForeignKey(sessao => sessao.CinemaId);
+
+        builder.Entity<Sessao>()
+            .HasOne(sessao => sessao.Filme)
+            .WithMany(filme => filme.sessoes)
+            .HasForeignKey(sessao => sessao.FilmeId);
+    }
 }
--- fanc02-relacionando-entidades/FilmesApi/Models/Sessao.cs
+++ fanc02-relacionando-entidades/FilmesApi/Models/Sessao.cs
@@ -4,12 +4,7 @@ namespace FilmesApi.Models;
 
 public class Sessao
 {
-    [Key]
-    [Required]
-    public int Id { get; set; }
-
-    [Required]
-    public int FilmeId { get; set; }
+    public int? FilmeId { get; set; }
 
     public virtual Filme Filme { get; set; }
 
